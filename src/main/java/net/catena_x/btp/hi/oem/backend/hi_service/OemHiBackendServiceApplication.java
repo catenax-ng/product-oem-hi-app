fd7e5ba67f27592ab12d473dad43463448df1a6e
afederl <anton.federl@pfeiffercontrol.de>
2023-01-27 17:12:10 +0100

    Deactivated S3SpringWrapper bean since the HttpUrl constructor parameter can not be converted.

--- src/main/java/net/catena_x/btp/hi/oem/backend/hi_service/OemHiBackendServiceApplication.java
+++ src/main/java/net/catena_x/btp/hi/oem/backend/hi_service/OemHiBackendServiceApplication.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 		"net.catena_x.btp.hi.supplier",
 		"net.catena_x.btp.libraries.notification",
 		"net.catena_x.btp.libraries.oem.backend.model.dto",
-		"net.catena_x.btp.libraries.oem.backend.cloud",
+//		"net.catena_x.btp.libraries.oem.backend.cloud",
 		"net.catena_x.btp.libraries.oem.backend.edc",
 		"net.catena_x.btp.libraries.util",
 		"net.catena_x.btp.libraries.edc",
